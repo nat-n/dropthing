89fbbc5008b5613c32f57509d8a9d9750e84cc19
Nat Noordanus <n@natn.me>
2015-02-02 16:36:55 +0000

    Added recovery resiliency against unstable connectivity

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 // var nodeNotifier = require('node-notifier');
 var path = require('path');
 var fs = require('fs');
-var open = require("open");
+// var open = require("open");
 var processManager = require('./lib/processManager');
 var config = require('./lib/config');
 var logger = require('./lib/logger');
@@ -14,17 +14,19 @@ var thingiverse = require('./lib/thingiverse');
 var notifier = require('./lib/notifier');
 
 
-function init(d) {
-
+function init() {
   // Initialize queues
   var Qs = {
     create: [],
     upload: [],
-    finalize: []
+    publish: []
   };
+
+  // TODO: load Qs from file if possible.
+
   processManager.init(Qs);
 
-  logger.info("DropThing Server ready");
+  thingiverse.ConnectionManager.init()
 
   // Start watching the target directory
   fs.watch(config.dropDir, function (change, filename) {
@@ -49,11 +51,11 @@ function init(d) {
         // Looks like the file was deleted,
         // find it's thing object mark it deleted and unqueue it.
         logger.log('verbose', 'File removed: ' + filename);
-        ['create', 'upload', 'finalize'].forEach(function (queueName) {
+        ['create', 'upload', 'publish'].forEach(function (queueName) {
           Qs[queueName] = Qs[queueName].filter(function (thing) {
             if (thing.filename === filename) {
-              logger.info('Deleting : "' + filename +
-                          '" from queue ' + queueName);
+              logger.info('Removed : "' + filename +
+                          '" with status ' + thing.status);
               thing.status = 'deleted';
               return false;
             }
@@ -66,6 +68,8 @@ function init(d) {
       logger.error('Change "' + change + '"detected without filename.');
     }
   });
+
+  logger.info("DropThing initialized watching directory " + config.dropDir);
 }
 
 function exitWithError(err) {
@@ -77,40 +81,7 @@ function server() {
   // TODO: launch http server, serving page with client side OAuth2 workflow
 }
 
-function authenticate(success, failure) {
-  if (config.accessToken && config.accessToken.length === 32) {
-    // Get user info just to verify that the access token works
-    thingiverse.getCurrentUser(success, function(res) {
-      if (res.code === 'ENOTFOUND' && res.syscall === 'getaddrinfo') {
-        // Couldn't reach host
-        notifier.unreachable();
-        logger.error("Couldn't reach thingiverse");
-        failure();
-
-        // NOW WHAT??? retry with backoff?
-        // should have a dedicated subroutine for this stuff
-
-      } else {
-        // Clear accessToken and try again to trigger user auth workflow.
-        config.accessToken = void 0;
-        authenticate(success, failure);
-      }
-    });
-
-  } else {
-
-    // TODO: serve a page that goes through the auth cycle and reports the
-    // accessToken to the server
-    // open("http://0.0.0.0:8888");
-
-    logger.error("No valid access token provided, it may have expired.");
-
-    failure();
-  }
-
-}
-
 // Bootstrap app
 logger.info("Initializing DropThing Server");
 // server();
-authenticate(init, exitWithError);
+init()
