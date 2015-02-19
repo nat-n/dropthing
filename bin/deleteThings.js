60b5309235c11c7bed847ecf2dd4f8aa1807764a
Nat Noordanus <n@natn.me>
2015-02-19 13:30:54 +0000

    improved reliability of deletion script

--- bin/deleteThings.js
+++ bin/deleteThings.js
@@ -6,40 +6,80 @@ var thingiverse = require('../lib/thingiverse');
 var logger = require('../lib/logger');
 
 
-// TODO: script that deletes all things by a user that meet certain criteria
-
-logger.info('Initialising deletion process');
-
-
+// Setup dependencies
 auth.startServer();
 thingiverse.ConnectionManager.init();
 
-// For some
-config.httpTimeout = 30000;
+// For some reason this helps
+config.httpTimeout = 40000;
 
-// Get list of all the users things
-thingiverse.thingsByUser(function (allThings) {
-  logger.info("Retrieved the user's things");
+// Parse argument
+var collectionName = process.argv[2];
+if (!collectionName) {
+  console.log('!! A collection id argument is required');
+  process.exit(1);
+}
 
-  // TODO: filter them by certain criterion (unless we really just want to delete everything??)
+logger.info('Initialising deletion process');
 
-  var markedThingIds = [];
-  allThings.forEach(function (thing) {
-    if (true) { // ????? everything ?????
-      markedThingIds.push(thing.id);
+// Lookup the collection Id for this collection name
+thingiverse.collectionsByUser(function(collections){
+  var found = false;
+  for (var i = collections.length - 1; i >= 0; i--) {
+    var collection = collections[i];
+    if (collection.name === collectionName) {
+      deleteCollectionContents(collection.id);
+      found = true;
     }
-  });
-
-  // Delete each one
-  markedThingIds.forEach(function (thingId) {
-    thingiverse.deleteThing(thingId, function () {
-      logger.info("Successfully deleted " + thingId);
-    }, function () {
-      logger.error("Failed to delete " + thingId);
-    })
-  });
-
-}, function (res) {
-  console.log(res);
-  logger.error("Failed to retrieve the user's things");
+  };
+  if (!found) {
+    console.log("!! No collection was found called: " + collectionName);
+    process.exit(1);
+  }
+}, function () {
+  console.log('!! Couldn\'t connect to thingiverse to lookup collection ID');
+  process.exit(1);
 });
+
+
+function deleteCollectionContents(collectionId) {
+  // Get list of all the things in this collection, and setup a queue that is
+  // checked every couple of seconds to ensure a few deletions are in progress
+  // until it is empty.
+  thingiverse.thingsInCollection(collectionId, function (deletionQ) {
+    logger.info("Retrieved "+deletionQ.length+" things from collection: " + collectionName);
+
+    setInterval(function () {
+      deletionQ.filter(function (thing) { return !thing.deleted; });
+
+      if (deletionQ.length === 0) {
+        console.log("Bulk deletion complete");
+        process.exit(0);
+      }
+      var activeCount = 0;
+
+      deletionQ.forEach(function (thing) {
+        if (activeCount > 3) {
+          return;
+        }
+        if (thing.deleting !== true) {
+          thingiverse.deleteThing(thing.id, function () {
+            logger.info("Successfully deleted " + thing.name + "(" + thing.id + ")");
+            thing.deleted = true;
+          }, function () {
+            logger.error("Failed to delete " + thing.name + "(" + thing.id + ")" +
+                         ", will try again shortly.");
+            thing.deleting = false;
+          })
+          thing.deleting = true;
+          activeCount++;
+        }
+      });
+
+    }, 2000)
+
+  }, function (res) {
+    console.log(res);
+    logger.error("Failed to retrieve the things in this collection");
+  });
+}
